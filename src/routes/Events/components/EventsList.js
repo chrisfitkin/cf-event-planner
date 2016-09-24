199a33af0ccaaf2e53ac88316b2e6683cc1b4439
chrisfitkin <cfitkin@gmail.com>
2016-09-24 07:20:33 -0700

    starting the create event form

--- src/routes/Events/components/EventsList.js
+++ src/routes/Events/components/EventsList.js
@@ -10,13 +10,16 @@ const renderEvents = (events) => {
   })
 }
 
+// Check this out for submit handling
+// https://github.com/chrisfitkin/react-redux-blog/blob/master/public/src/components/PostsForm.js
 export const EventsList = (props) => {
   const events = renderEvents(props.eventsList.events)
   return (
     <div style={{ margin: '0 auto' }} >
       <h2>Event List:</h2>
+      <input type="text" className="form-control" placeholder="Need to wire this textfield to submit handler..." />
       <button className='btn btn-default' onClick={props.createEvent}>
-        Increment
+        Add Event
       </button>
       <div>
         {events}
