c8d8f34f338f38ba238292662dd461441be83db6
chrisfitkin <cftkin@gmail.com>
2016-09-26 23:22:55 -0700

    styled material-ui form, wired first field

--- src/routes/Events/components/EventForm.js
+++ src/routes/Events/components/EventForm.js
@@ -5,6 +5,9 @@ import moment from 'moment'
 import TextField from 'material-ui/TextField'
 import DatePicker from 'material-ui/DatePicker'
 import TimePicker from 'material-ui/TimePicker'
+import AutoComplete from 'material-ui/AutoComplete'
+import MenuItem from 'material-ui/MenuItem';
+import AddressAutocomplete from './AddressAutocomplete'
 
 // Check this out for submit handling
 // https://github.com/chrisfitkin/react-redux-blog/blob/master/public/src/components/PostsForm.js
@@ -12,60 +15,58 @@ export const EventForm = (props) => {
   let title, host, eventType, startDate, startTime, endDate, endTime, guestList, location, message
   let today = new Date();
   let todayFormatted = moment(today).format('MM/DD/YYYY');
+
+  const eventTypeOptions = ["Birthday Party", "Conference", "Wedding", "Dinner", "Meet Up", "Work Meeting", "Drinks", "Baseball Game"]
+
+  const formSubmit = e => {
+    e.preventDefault()
+    // if (!title.value.trim()) {
+    //   return
+    // }
+    console.log(props)
+    props.createEvent(title.input.value)
+    // TODO: Clear Fields
+    //title.input.value = ''
+    // TODO: go to list route
+    title.focus()
+  }
+
   return (
     <div>
-      <form onSubmit={e => {
-        e.preventDefault()
-        if (!title.value.trim()) {
-          return
-        }
-        props.createEvent(title.value)
-        title.value = ''
-        title.focus()
-      }}>
+      <form onSubmit={formSubmit}>
         <TextField
+          floatingLabelText="Name your event"
           hintText="A day at the beach"
-          floatingLabelText="Name of your event"
           autoComplete="title"
           autoFocus
           required
-          ref={ref => { title = ref}}
-        />
-        <label>
-          <span>Type of event</span>
-          <input
-            type="text"
-            name='host'
-            ref={ref => { eventType = ref }}
-            className="form-control"
-            placeholder="Birthday, Meet Up, etc..."
-            list="eventTypes"
-            />
-            <datalist id="eventTypes">
-              <option value="Birthday Party" />
-              <option value="Conference" />
-              <option value="Wedding" />
-              <option value="Dinner" />
-              <option value="Meet Up" />
-              <option value="Work Meeting" />
-              <option value="Drinks" />
-              <option value="Baseball Game" />
-            </datalist>
-        </label>
-        <label>
-          <span>Who is hosting</span>
-          <input
-            type="text"
-            name='host'
-            ref={ref => { host = ref }}
-            className="form-control"
-            placeholder="John Smith or Acme, Co."
-            autoComplete="name"
-            />
-        </label>
+          ref={node => { title = node}}
+        /><br/>
+        <AutoComplete
+          floatingLabelText="Type of event"
+          hintText="Birthday, Meet Up, etc..."
+          filter={AutoComplete.fuzzyFilter}
+          openOnFocus={true}
+          dataSource={eventTypeOptions}
+          required
+          ref={node => { eventType = node }}
+        /><br/>
+        <AddressAutocomplete
+          floatingLabelText="Where is it"
+          hintText="123 Main Street, Los Angeles, CA"
+          required
+          ref={node => { location = node }}
+        /><br/>
+        <TextField
+          hintText="John Smith or Acme, Co."
+          floatingLabelText="Who is hosting"
+          autoComplete="name"
+          required
+          ref={node => { host = node}}
+        /><br/>
         <DatePicker
           autoOk
-          floatingLabelText="When is your event"
+          floatingLabelText="What day"
           type="text"
           name='startDate'
           ref={ref => { startDate = ref }}
@@ -74,14 +75,14 @@ export const EventForm = (props) => {
         <TimePicker
           hintText="12hr Format"
           autoOk
+          floatingLabelText="What time"
           name="startTime"
           ref={ref => { startTime = ref }}
-          hintText="12:00 pm"
           format="ampm"
         />
         <DatePicker
           autoOk
-          floatingLabelText="When does it end"
+          floatingLabelText="Ending day"
           type="text"
           name='endDate'
           ref={ref => { endDate = ref }}
@@ -92,7 +93,7 @@ export const EventForm = (props) => {
           autoOk
           name="endTime"
           ref={ref => { endTime = ref }}
-          hintText="12:00 pm"
+          floatingLabelText="Ending time"
           format="ampm"
         />
         <br/>
